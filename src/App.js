0747ed010e7f795ebdef0cb56480d7fb65b76557
Ashutosh Soni <viknat1994@gmail.com>
2020-01-12 14:29:44 +0530

    implemented createSturctured selector and refactor the code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from "reselect";
 
 import './App.css';
 
@@ -10,7 +11,7 @@ import SignInAndSignUpPage  from './pages/sign-in-and-sign-up/sign-in-and-sign-u
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/users/user.actions';
-
+import {selectCurrentUser } from './redux/users/user.selectors'
 
 class App extends React.Component {
   
@@ -66,8 +67,8 @@ class App extends React.Component {
     
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 
